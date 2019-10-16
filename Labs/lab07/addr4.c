ebfdeabd599aaa0efac6506fecd6a224a50e478f
NeverBehave <gayhub@never.pet>
2019-10-15 21:36:50 -0400

    lab07 finished

--- Labs/lab07/addr4.c
+++ Labs/lab07/addr4.c
@@ -13,7 +13,7 @@ int add_two_bits(int d0, int d1, int carry_in, int *carry_out)
 {
     /* FILL IN CODE HERE! */
     if (carry_out) // NULL checking
-        *carry_out = (d0 & d1) + (carry_in & (d0 | d1));
+        *carry_out = (d0 & d1) | (carry_in & (d0 | d1));
 
     return d0 ^ d1 ^ carry_in;
 }
@@ -27,7 +27,7 @@ int main()
     int ci[4] = { 0, 0, 0, 0 };
     int sum[4] = { 0, 0, 0, 0 };
 
-    for (int i = 0; i < 4; i++) {
+    for (i = 3; i >= 0; i--) {
         if (i - 1 >= 0) {
             sum[i] = add_two_bits(d0[i], d1[i], ci[i - 1], &ci[i]);
         } else {
@@ -36,17 +36,17 @@ int main()
 
     }
 
-    for (int i = 0; i < 4; i++)
+    for (i = 3; i >= 0; i--)
     {
         printf("%d ",d0[i]);
     }
     printf("+");
-    for (int i = 0; i < 4; i++)
+    for (i = 3; i >= 0; i--)
     {
         printf(" %d",d1[i]);
     }
     printf(" =");
-    for (int i = 0; i < 4; i++)
+    for (i = 3; i >= 0; i--)
     {
         printf(" %d",sum[i]);
     }
