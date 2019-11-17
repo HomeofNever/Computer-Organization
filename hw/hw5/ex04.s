4beecee19644ede78b58986f2fff885e4fbf4e5d
NeverBehave <gayhub@never.pet>
2019-11-17 11:06:22 -0500

    hw5: finished

---
+++ hw/hw5/ex04.s
@@ -0,0 +1,4 @@
+and $s1,$s0,$s0
+sub $t2,$s0,$s5
+addi $t4,$s3,-70
+ori $t4,$s1,73
--- hw/hw5/main.c
+++ hw/hw5/main.c
@@ -3,10 +3,13 @@
 #include <string.h>
 #include <stdbool.h>
 
+// MAX
 #define MAX_LINES 5
-#define MAX_CYCLE 8
+#define MAX_CYCLE 9
 #define MAX_LINE_LENGTH 128
 #define MAX_INST_LENGTH 5
+#define MAX_STAGE 5
+#define MAX 128
 // Types
 #define IN_ADD "add"
 #define IN_ADDI "addi"
@@ -17,13 +20,8 @@
 #define IN_ORI "ori"
 #define IN_LW "lw"
 #define IN_SW "sw"
-// Registers
-#define REG_NUM 0
-#define REG_T 1
-#define REG_S 2
-#define REG_A 3
-#define REG_ZERO 3
 // Syntax
+#define ENDL "\0"
 #define SYB_SPACE ' '
 #define SYB_DOLLAR '$'
 #define SYB_COMMA ','
@@ -32,245 +30,87 @@
 // Init value
 #define UNREG -1
 // Boundaries
-#define MAX_T_REG 9
-
-// A record is a number - immediate value(int), a $s register(for vars), or a $t register(for temp).
-struct Record {
-    int type;
-    int data;
-};
+#define NOP1 "nop1"
+#define NOP2 "nop2"
 
 struct Instruction {
-    struct Record dependency1;
-    struct Record dependency2;
-    struct Record output;
-    int started_at;
-    int nops_to;
-    int nops_num;
-    int stage;
-    int out;
-    int dep;
-} Instruction_default = {
-        .dependency1={.type=UNREG, .data=UNREG},
-        .dependency2={.type=UNREG, .data=UNREG},
-        .output={.type=UNREG, .data=UNREG},
-        .nops_to=UNREG,
-        .nops_num= UNREG,
-        .out = UNREG,
-        .dep = UNREG,
-        .stage = 0,
-        .started_at = UNREG, // How many dot before exec
+    char mips[MAX_LINE_LENGTH];
+    char operator[MAX_INST_LENGTH];
+    char d1[MAX_INST_LENGTH];
+    char d2[MAX_INST_LENGTH];
+    char o[MAX_INST_LENGTH];
+} default_instruction = {
+        .operator = ENDL,
+        .mips = ENDL,
+        .d1 = ENDL,
+        .d2 = ENDL,
+        .o = ENDL
 };
 
+struct Line {
+    struct Instruction instruction;
+    char cols[MAX_CYCLE][MAX_INST_LENGTH];
+    int stage;
+    int started_at;
+    int nop_at;
+};
+
+struct Line board[MAX];
 char stages[5][4] = {"IF", "ID", "EX", "MEM", "WB"};
-char nop_1[5][4] = {"IF", "ID", "*", "*", "*"};
-char nop_2[6][4] = {"IF", "ID", "ID", "*", "*", "*"};
-int num_of_stages = 5;
-// Global Variables
-// Just dont want to pass them around
-char mips[MAX_LINES][MAX_LINE_LENGTH];
-int mips_line = 0;
-struct Instruction instructions[MAX_LINES]; // Including possible NOPs
-int cycle = 0;
+char nop_1_stage[5][4] = {"IF", "ID", "*", "*", "*"};
+char nop_2_stage[6][4] = {"IF", "ID", "ID", "*", "*", "*"};
+int line_num = 0;
+int real_exec_line = 0;
+int current_cycle = 0;
 
 
-// Print Helper
+bool not_empty(const char *c) {
+  return c[0] != '\0';
+}
 
-// Print Same Given Stage
-void print_stages(int num, char *stage) {
-  for (int i = 0; i < num; i++) {
-    printf("%s\t", stage);
+bool is_nop_1(struct Line *l) {
+  return strcmp(l->instruction.operator, NOP1) == 0;
+}
+
+bool is_nop_2(struct Line *l) {
+  return strcmp(l->instruction.operator, NOP2) == 0;
+}
+
+void initialize_line(struct Line *l) {
+  l->instruction = default_instruction;
+  l->stage = 0;
+  l->started_at = 0;
+  l->nop_at = UNREG;
+  for (int i = 0; i < MAX_CYCLE; ++i) {
+    strcpy(l->cols[i], ".");
   }
 }
 
-void print_star(int num) {
-  for (int i = 0; i < num; i++) {
-    printf("*\t");
-  }
+void init() {
+  for (int i = 0; i < MAX; i++)
+    initialize_line(&board[i]);
 }
 
-void print_newline() {
-  printf("\n");
+bool compare_dep(struct Instruction *i, char *d1, char *d2) {
+  return (not_empty(d1) && strcmp(i->o, d1) == 0) ||
+         (not_empty(d2) && strcmp(i->o, d2) == 0);
 }
 
-void print_dot(int num) {
-  for (int i = 0; i < num; i++) {
-    printf(".\t");
-  }
+bool has_dep(struct Instruction *i, char *d) {
+  return not_empty(d) &&
+         ((not_empty(i->d1) && strcmp(i->d1, d) == 0) ||
+          (not_empty(i->d2) && strcmp(i->d2, d) == 0));
 }
 
-// Print stage by given order
-void print_stage(int from, int to) {
-  for (int i = from; i <= to; i++) {
-    printf("%s\t", stages[i]);
-  }
-}
-
-void print_nop() {
-  printf("nop\t");
-}
-
-void print_nop1_stage(int started_at) {
-  print_nop();
-  int resp = cycle - started_at;
-  if (resp > 4) {
-    for (int i = 0; i <= 4; i++) {
-      printf("%s\t", nop_2[i]);
-    }
-    print_dot(resp - 4);
-  } else {
-    for (int i = 0; i <= resp; i++) {
-      printf("%s\t", nop_2[i]);
-    }
-  }
-}
-
-void print_nop2_stage(int started_at) {
-  print_nop();
-  int resp = cycle - started_at;
-  if (resp > 5) {
-    for (int i = 0; i <= 4; i++) {
-      printf("%s\t", nop_2[i]);
-    }
-    print_dot(resp - 5);
-  } else {
-    for (int i = 0; i <= resp; i++) {
-      printf("%s\t", nop_2[i]);
-    }
-  }
-}
-
-void print_rest() {
-  // Where we have not yet reached
-  print_dot(MAX_CYCLE - cycle);
-  print_newline();
-}
-
-void print_normal_instruction(struct Instruction *i) {
-  // How many cycle we need to cover?
-  int resp = cycle - i->started_at;
-  // What stage we have
-  // Normal(0-1) - (if)Dup - Normal(2-4) - Rest
-  if (resp > 2) {
-    print_stage(0, 1);
-    resp -= 2;
-    int n = i->nops_num;
-    // Dup
-    if (n > 0) {
-      if (resp > 2) {
-        print_stages(resp - 2, "ID");
-        resp -= 2;
-      } else {
-        print_stages(resp, "ID");
-        resp = 0;
-      }
-    }
-
-    // Normal
-    if (resp > 3) {
-      print_stage(2, 4);
-      print_dot(resp - 3);
-    } else {
-      print_stage(2, resp);
-    }
-  } else {
-    print_stage(0, resp - 1);
-  }
-}
-
-void increase_stage(struct Instruction *i) {
-  if (i->stage < num_of_stages - 1) {
-    i->stage++; // No more than 5
-  }
-}
-
-void init_array() {
-  for (int i = 0; i < MAX_LINES; i++) {
-    instructions[i] = Instruction_default;
-  }
-}
-
-bool same_reg(struct Record *a, struct Record *b) {
-  if (a->data == b->data &&
-      a->type == b->type)
-    return true;
-  else
-    return false;
-}
-
-bool compare_instruction_record(struct Instruction *i, struct Record *d1, struct Record *d2) {
-  return same_reg(&(i->output), d1) || same_reg(&(i->output), d2);
-}
-
-int string_to_type(char s) {
-  if (s == 's') {
-    return REG_S;
-  } else if (s == 't') {
-    return REG_T;
-  } else if (s == 'a') {
-    return REG_A;
-  }
-  {
-    fprintf(stderr, "Unknown String to Type: %c\n", s);
-    return UNREG;
-  }
-}
-
-int string_to_int(const char *s) {
-  return atoi(s);
-}
-
-bool print_one_stimulation() {
-  printf("CPU Cycles ===>\t1\t2\t3\t4\t5\t6\t7\t8\t9\n");
-  // The first line will always the same
-  printf("%s\t", mips[0]);
-  print_stage(0, instructions[0].stage);
-  // Where we have not yet reached
-  print_dot(MAX_CYCLE - cycle);
-  print_newline();
-
-  // Rest
-  for (int i = 1; i < mips_line; i++) {
-    printf("%s\t", mips[i]);
-    // For Each Cycles, calculate:
-    int start = instructions[i].started_at;
-    // We need to start running
-    if (cycle >= start) {
-      int st = instructions[i].stage;
-      if (st == 1) {
-        // We need to detect if the nop
-        if (instructions[i].nops_num != UNREG) {
-          // First Nop
-          // Delays
-          print_dot(start);
-          print_nop1_stage(start);
-          print_rest();
-          if (instructions[i].nops_num == 2) {
-            // Delays
-            print_nop();
-            print_nop2_stage(start);
-            print_rest();
-          } // EOF NOPS
-        }
-      } // END of NOPS identify
-
-      // delay
-      print_dot(start);
-      int real_int = print_normal_instruction(&instructions[i]); // Let the function handle
-      print_newline();
-
-      // Increase stage
-      if (instructions[i - 1].stage - instructions[i].stage > 1) {
-        increase_stage(&instructions[i]);
-      }
-    }
-  }
-
-  increase_stage(&instructions[0]);
-  cycle++;
-
-  return instructions[mips_line - 1].stage == 4;
-}
+//int find_last_real_inst(int i) {
+//  for (int j = i; j >= 0; j--) {
+//    if (!(is_nop_2(&board[j]) || is_nop_1(&board[j]))) {
+//      return j;
+//    }
+//  }
+//
+//  return UNREG;
+//}
 
 void read_instruction(const char *line, unsigned long *i, char *inst, unsigned long strLen) {
   unsigned int index = 0;
@@ -286,7 +126,7 @@ void read_instruction(const char *line, unsigned long *i, char *inst, unsigned l
   }
 }
 
-void read_parts(const char *line, unsigned long *i, struct Record *reg, unsigned long strLen) {
+void read_parts(const char *line, unsigned long *i, char *reg, unsigned long strLen) {
   // or $s0,$s0,$t3
   // lw $a0,12($sp)
   unsigned long j;
@@ -294,8 +134,9 @@ void read_parts(const char *line, unsigned long *i, struct Record *reg, unsigned
     if (line[j] == SYB_DOLLAR) {
       // Here we have a Register
       // Read next two
-      reg->type = string_to_type(line[j + 1]);
-      reg->data = string_to_int(&line[j + 2]);
+      reg[0] = line[j + 1];
+      reg[1] = line[j + 2];
+      reg[2] = '\0';
       j += 3;
       break;
     }
@@ -306,45 +147,39 @@ void read_parts(const char *line, unsigned long *i, struct Record *reg, unsigned
 
 
 void read_mips(FILE *file) {
-  while (fgets(mips[mips_line], MAX_LINE_LENGTH, file)) {
-    struct Record dependency_1 = {.type = UNREG, .data = UNREG};
-    struct Record dependency_2 = {.type = UNREG, .data = UNREG};
-    struct Record output = {.type = UNREG, .data = UNREG};
+//  struct Line *l = &board[line_num];
+  char current_line[MAX_LINE_LENGTH];
+  while (fgets(current_line, MAX_LINE_LENGTH, file)) {
     // Remove the last newline
-    mips[mips_line][strcspn(mips[mips_line], "\n")] = '\0';
-    unsigned long strLen = strlen(mips[mips_line]);
+    current_line[strcspn(current_line, "\n")] = '\0';
+    unsigned long strLen = strlen(current_line);
 
     // Parsing line
     unsigned long i = 0;
-    int dep = UNREG;
-    int out = UNREG;
+    char o[MAX_INST_LENGTH];
+    char d1[MAX_INST_LENGTH];
+    char d2[MAX_INST_LENGTH];
     // Read Instruction type
     char inst[MAX_INST_LENGTH];
-    read_instruction(mips[mips_line], &i, inst, strLen);
+    read_instruction(current_line, &i, inst, strLen);
 
     // Identify type
     if (strcmp(inst, IN_ADD) == 0 ||
         strcmp(inst, IN_AND) == 0 ||
         strcmp(inst, IN_OR) == 0 ||
         strcmp(inst, IN_SUB) == 0) {
-      read_parts(mips[mips_line], &i, &output, strLen);
-      read_parts(mips[mips_line], &i, &dependency_1, strLen);
-      read_parts(mips[mips_line], &i, &dependency_2, strLen);
-      dep = 2;
-      out = 1;
+      read_parts(current_line, &i, o, strLen);
+      read_parts(current_line, &i, d1, strLen);
+      read_parts(current_line, &i, d2, strLen);
     } else if (strcmp(inst, IN_ADDI) == 0 ||
                strcmp(inst, IN_LW) == 0 ||
                strcmp(inst, IN_ANDI) == 0 ||
                strcmp(inst, IN_ORI) == 0) {
-      read_parts(mips[mips_line], &i, &output, strLen);
-      read_parts(mips[mips_line], &i, &dependency_1, strLen);
-      dep = 1;
-      out = 1;
+      read_parts(current_line, &i, o, strLen);
+      read_parts(current_line, &i, d1, strLen);
     } else if (strcmp(inst, IN_SW) == 0) {
-      read_parts(mips[mips_line], &i, &dependency_1, strLen);
-      read_parts(mips[mips_line], &i, &dependency_2, strLen);
-      out = 0;
-      dep = 2;
+      read_parts(current_line, &i, d1, strLen);
+      read_parts(current_line, &i, d2, strLen);
     } else {
       fprintf(stderr, "Unknown Reg type: '%s'\n", inst);
     } // End of Var detect
@@ -355,31 +190,130 @@ void read_mips(FILE *file) {
     // or $s0,$s0,$t3
     // lw $a0,12($sp)
     // sw $t6,32($a1)
-    int nops = UNREG;
-    int nops_to = UNREG;
-    for (int j = mips_line - 1; j >= 0; j--) {
-      if (instructions[j].out > 0) {
+    int nops_at = UNREG;
+    for (int j = line_num - 1; j >= 0; j--) {
+      if (!(is_nop_1(&board[j]) && is_nop_2(&board[j]))) {
         // Find if there is a dependency
-        if (compare_instruction_record(&instructions[j], &dependency_1, &dependency_2)) {
+        if (compare_dep(&board[j].instruction, d1, d2)) {
           // Compute distance
-          int dist = mips_line - instructions[j].started_at;
-          nops = dist - 1;
-          nops_to = mips_line;
-          break;
+          int dist = line_num - board[j].started_at;
+          if (dist > 0 && dist < 3) {
+            // One bubble
+            // Wait! Check if it is waited
+            // If the dep is not the nearest line, the last line may have waited for the dep for you
+            if (j == line_num - 1 ||
+                !has_dep(&board[line_num - 1].instruction, board[j].instruction.o)
+                    ) {
+              nops_at = j;
+              strcpy(board[line_num].instruction.operator, "nop1");
+              board[line_num].started_at = real_exec_line;
+              line_num++;
+              if (dist == 1) {
+                strcpy(board[line_num].instruction.operator, "nop2");
+                board[line_num].started_at = real_exec_line;
+                line_num++;
+              }
+            }
+          }
+          break; // Dep Found, if it is not qualify, then we don't need to worried about it anymore.
         }
       }
     }
     // Assign current line
-    instructions[mips_line].started_at = mips_line;
-    instructions[mips_line].dependency1 = dependency_1;
-    instructions[mips_line].dependency2 = dependency_2;
-    instructions[mips_line].output = output;
-    instructions[mips_line].nops_to = nops_to;
-    instructions[mips_line].nops_num = nops;
-    instructions[mips_line].out = out;
-    instructions[mips_line].dep = dep;
-    mips_line++;
+    strcpy(board[line_num].instruction.mips, current_line);
+    strcpy(board[line_num].instruction.operator, inst);
+    strcpy(board[line_num].instruction.d1, d1);
+    strcpy(board[line_num].instruction.d2, d2);
+    strcpy(board[line_num].instruction.o, o);
+    board[line_num].started_at = real_exec_line;
+    board[line_num].nop_at = nops_at;
+    line_num++;
+    real_exec_line++;
   } // Finish Parsing
+}
+
+void print_line(struct Line *l) {
+  // Current Cycle, Each of them have their own stages
+  if (is_nop_1(l)) {
+    if (current_cycle > l->started_at + 1) {
+      printf("nop\t\t");
+      for (int i = 0; i < MAX_CYCLE - 1; i++) {
+        printf("%s\t", l->cols[i]);
+      }
+      printf("%s", l->cols[MAX_CYCLE - 1]);
+      printf("\n");
+    }
+  } else if (is_nop_2(l)) {
+    if (current_cycle > l->started_at + 2) {
+      printf("nop\t\t");
+      for (int i = 0; i < MAX_CYCLE - 1; i++) {
+        printf("%s\t", l->cols[i]);
+      }
+      printf("%s", l->cols[MAX_CYCLE - 1]);
+      printf("\n");
+    }
+  } else {
+    printf("%s\t", l->instruction.mips);
+    for (int i = 0; i < MAX_CYCLE - 1; i++) {
+      printf("%s\t", l->cols[i]);
+    }
+    printf("%s", l->cols[MAX_CYCLE - 1]);
+    printf("\n");
+  }
+}
+
+void print_all() {
+  printf("CPU Cycles ===>\t1\t2\t3\t4\t5\t6\t7\t8\t9\n");
+  for (int i = 0; i < line_num; i++) {
+    print_line(&board[i]);
+  }
+  printf("\n");
+}
+
+bool run_one_stimulation() {
+  // First line
+  // #5 will be the final
+  if (board[0].stage < MAX_STAGE) {
+    strcpy(board[0].cols[current_cycle], stages[board[0].stage]);
+    board[0].stage++;
+  }
+
+  // Rest
+  for (int i = 1; i < line_num; i++) {
+    if (current_cycle >= board[i].started_at) {
+      if (is_nop_2(&board[i])) {
+        if (board[i].stage < MAX_STAGE + 1) {
+          strcpy(board[i].cols[current_cycle], nop_2_stage[board[i].stage]);
+          board[i].stage++;
+        }
+      } else if (is_nop_1(&board[i])) {
+        if (board[i].stage < MAX_STAGE) {
+          strcpy(board[i].cols[current_cycle], nop_1_stage[board[i].stage]);
+          board[i].stage++;
+        }
+      } else {
+        if (board[i].stage < MAX_STAGE) {
+          // Check dep
+          if (board[i].nop_at != UNREG) {
+            if (board[board[i].nop_at].stage != 5 && board[i].stage == 1) {
+              strcpy(board[i].cols[current_cycle], stages[board[i].stage]);
+            } else {
+              strcpy(board[i].cols[current_cycle], stages[board[i].stage]);
+              board[i].stage++;
+            }
+          } else {
+            strcpy(board[i].cols[current_cycle], stages[board[i].stage]);
+            if (board[i - 1].stage == 5 || board[i - 1].stage - board[i].stage > 1) {
+              // If the previous block, wait for it
+              board[i].stage++;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return board[line_num - 1].stage == 5;
 }
 
 int main(int argc, char *argv[]) {
@@ -400,21 +334,21 @@ int main(int argc, char *argv[]) {
   }
 
   // Init
-  init_array();
+  init();
 
   // Processing & Dependency Check
   read_mips(file);
 
   printf("START OF SIMULATION\n");
   printf("\n");
-  for (int i = 0; i < MAX_CYCLE; i++) {
-    if (print_one_stimulation()) {
+  for (; current_cycle < MAX_CYCLE; current_cycle++) {
+    if (run_one_stimulation()) {
+      print_all();
       break;
     }
+    print_all();
   }
-  printf("\n");
   printf("END OF SIMULATION");
-
 
   fclose(file);
 
