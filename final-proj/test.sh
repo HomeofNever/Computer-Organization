16393ecb616a511308549bce786266d1b9f3d458
NeverBehave <gayhub@never.pet>
2019-12-08 01:09:00 -0500

    update

---
+++ final-proj/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+python3 main.py NF test/p1-input01.txt > temp/p1-output01.txt
+python3 main.py NF test/p1-input02.txt > temp/p1-output02.txt
+python3 main.py F test/p1-input03.txt > temp/p1-output03.txt
+python3 main.py F test/p1-input04.txt > temp/p1-output04.txt
+python3 main.py NF test/p1-input05.txt > temp/p1-output05.txt
+python3 main.py NF test/p1-input06.txt > temp/p1-output06.txt
+python3 main.py NF test/p1-input07.txt > temp/p1-output07.txt
+python3 main.py F test/p1-input08.txt > temp/p1-output08.txt
+python3 main.py F test/p1-input09.txt > temp/p1-output09.txt
